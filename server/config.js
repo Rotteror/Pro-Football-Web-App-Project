a628d935104f07710cfe2d7ee8e80696b1c95fe4
Rotteror <xeziod@gmail.com>
2021-10-01 17:45:03 +0300

    Add User Model, Error Parser

---
+++ server/config.js
@@ -0,0 +1,6 @@
+const authCookieName = 'AUTH_COOKIE';
+
+module.exports = {
+    SECRET: 'secret2021',
+    authCookieName
+};
---
+++ server/utils/errorParser.js
@@ -0,0 +1,9 @@
+function parserError(err) {
+    if (err.name == 'ValidationError') {
+        return Object.values(err.errors).map(e => e.properties.message).join(' \n');
+    } else {
+        return err.message;
+    }
+}
+
+module.exports = parserError;
