8a4fc03746ce9509b1245e2c1c920243577d37a5
Rotteror <xeziod@gmail.com>
2021-10-07 13:58:41 +0300

    Added Matches Page/ Routing

---
+++ src/app/matches/matches-routing.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { MatchListComponent } from './match-list/match-list.component';
+
+
+
+const routes: Routes = [
+    {
+        path: 'matches',
+        component: MatchListComponent,
+    }
+]
+@NgModule({
+    imports: [RouterModule.forChild(routes)],
+    exports: [RouterModule]
+})
+export class MatchesRoutingModule { }
--- src/app/matches/matches.module.ts
+++ src/app/matches/matches.module.ts
@@ -2,20 +2,25 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { BookieCardsComponent } from './bookie-cards/bookie-cards.component';
 import { MatchPickComponent } from './match-pick/match-pick.component';
+import { MatchListComponent } from './match-list/match-list.component';
+import { MatchesRoutingModule } from './matches-routing.module';
 
 
 
 @NgModule({
   declarations: [
     BookieCardsComponent,
-    MatchPickComponent
+    MatchPickComponent,
+    MatchListComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    MatchesRoutingModule
   ],
   exports: [
     BookieCardsComponent,
-    MatchPickComponent
+    MatchPickComponent,
+    MatchListComponent
   ]
 })
 export class MatchesModule { }
