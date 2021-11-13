837d83dada830c4e8e45de4768df046e113de361
Rotteror <xeziod@gmail.com>
2021-11-13 17:45:56 +0200

    Enabled Anchro Scrolling

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './core/home/home.component';
+import { HowItWorksComponent } from './core/how-it-works/how-it-works.component';
 
 
 
@@ -8,16 +9,26 @@ const routes: Routes = [
   {
     path: '',
     pathMatch: 'full',
-    redirectTo: '/home'
+    redirectTo: '/home',
   },
   {
     path: 'home',
+    component: HomeComponent,
+  },
+  {
+    path: 'how',
     component: HomeComponent
   }
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes,
+    {
+      anchorScrolling: 'enabled',
+      scrollPositionRestoration: 'enabled',
+      scrollOffset: [0, 60],
+
+    })],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
